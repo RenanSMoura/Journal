295d6104d80e58ff3dc14b6322d8396f536bd040
RenanSMoura <renan.silva.moura@gmail.com>
2018-03-10 20:52:50 -0300

    Add Base Activity

---
+++ app/src/main/java/moura/silva/com/journal/activity/BaseActivity.kt
@@ -0,0 +1,51 @@
+package moura.silva.com.journal.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v4.app.FragmentActivity
+import android.util.Log
+import moura.silva.com.journal.utils.Extra
+
+/**
+ * Created by Renan on 10/03/2018.
+ */
+abstract class BaseActivity : FragmentActivity(){
+
+    protected abstract val TAG : String
+
+    protected abstract fun getLayout() : Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayout())
+        Log.v(TAG, Extra.ON_CREATE)
+    }
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        Log.v(TAG,Extra.ON_POST_CREATE)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(TAG,Extra.ON_RESTART)
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        Log.v(TAG,Extra.ON_POST_RESUME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(TAG,Extra.ON_PAUSE)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(TAG,Extra.ON_STOP)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG,Extra.ON_DESTROY)
+    }
+}
--- app/src/main/java/moura/silva/com/journal/activity/MainActivity.kt
+++ app/src/main/java/moura/silva/com/journal/activity/MainActivity.kt
@@ -1,50 +1,10 @@
 package moura.silva.com.journal.activity
 
-import android.os.Bundle
-import android.os.PersistableBundle
-import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import moura.silva.com.journal.R
-import moura.silva.com.journal.utils.Extra
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-    companion object {
-        val TAG = this::class.java.name
-    }
+    override val TAG = "MainActivity"
+    override fun getLayout(): Int  = R.layout.activity_main
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.v(TAG, Extra.ON_CREATE)
-    }
-
-    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onPostCreate(savedInstanceState, persistentState)
-        Log.v(TAG,Extra.ON_POST_CREATE)
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.v(TAG,Extra.ON_RESTART)
-    }
-
-    override fun onPostResume() {
-        super.onPostResume()
-        Log.v(TAG,Extra.ON_POST_RESUME)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.v(TAG,Extra.ON_PAUSE)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.v(TAG,Extra.ON_STOP)
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.v(TAG,Extra.ON_DESTROY)
-    }
 }
